d1035a20e5b442c7ef32480eb922d9bfd9ea4ba9
Sumanta <sumanta>
2016-10-15 12:21:21 +0530

    added application properties

--- springbootex/src/test/java/com/springbootex/SampleCassandraApplicationTest.java
+++ springbootex/src/test/java/com/springbootex/SampleCassandraApplicationTest.java
@@ -8,7 +8,6 @@ import java.util.UUID;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -18,6 +17,7 @@ import com.springbootex.cass.repositories.HotelRepository;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SpringbootexApplication.class)
 public class SampleCassandraApplicationTest {
+	
 	@Autowired
 	private HotelRepository hotelRepository;
 
