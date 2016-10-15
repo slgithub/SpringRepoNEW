69f847d3e374b265e0e4b069a062e5cb85ef068b
Sumanta <sumanta>
2016-10-15 17:19:57 +0530

    change in testcase

--- springbootex/src/test/java/com/springbootex/SpringbootexApplicationTests.java
+++ springbootex/src/test/java/com/springbootex/SpringbootexApplicationTests.java
@@ -2,12 +2,13 @@ package com.springbootex;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SpringbootexApplication.class)
+@SpringBootTest
 @WebAppConfiguration
 public class SpringbootexApplicationTests {
 
