6072f048c9aff7b13b6e98681b66f643a570e1fa
Sumanta <sumanta.laha@wipro.com>
2017-04-16 23:28:33 +0530

    kafka producer change commit

--- spring-kafka-example/src/main/java/com/example/kafka/KafkaProducerConfig.java
+++ spring-kafka-example/src/main/java/com/example/kafka/KafkaProducerConfig.java
@@ -33,9 +33,9 @@ public class KafkaProducerConfig {
 		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
 		props.put(ProducerConfig.RETRIES_CONFIG, 2);
 		props.put(ProducerConfig.ACKS_CONFIG, "1");
-		props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
-		props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
-		props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
+		//props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+		//props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+		//props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
 		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		return props;
